d5b31c4576675c4f9c645ba4a7f865020d94cf48
AYOBAMIINSTINCT <111369195+AYOBAMIINSTINCT@users.noreply.github.com>
2023-05-11 02:45:01 -0700

    lclear clear git commit -m 'Post a Job page' post a job page

---
+++ PAGES/Post a Job.js
@@ -0,0 +1,6 @@
+// JavaScript code to handle button click event
+const applyButton = document.querySelector('.apply-button');
+
+applyButton.addEventListener('click', function() {
+  console.log('Post Jobs button clicked');
+});
